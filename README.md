49515fb6663d2eec22baa306d254ec6104e64661
infalogger <oskaras.sirvys@gmail.com>
2025-04-15 23:39:12 +0200

    finished implementing everything, base Chuck Norris joke system works now

---
+++ README.md
@@ -0,0 +1,6 @@
+A simple Golang script that utilizes an api to tell the user Chuck Norris jokes.
+REQUIRES AN INTERNET CONNECTION TO USE!!!
+
+
+USAGE:
+Open the application and type y or yes for a chuck norris joke. Type no to exit
--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
+type Joke struct {
+	Value string `json:"value"`
+}
+
 func main() {
-	fmt.Println("hey")
+	fmt.Println("CHUCK NORRIS JOKE MACHINE")
+	isfirstjoke := true
+
+	for {
+		if isfirstjoke {
+			fmt.Println("Would you like to hear a Chuck Norris joke? (y/n): ")
+		} else {
+			fmt.Println("Would you like to hear another? (y/n): ")
+		}
+
+		var input string
+		fmt.Scanln(&input)
+
+		if input == "y" || input == "yes" {
+			isfirstjoke = false
+			reqjoke, _ := http.Get("https://api.chucknorris.io/jokes/random")
+			defer reqjoke.Body.Close()
+			body, _ := ioutil.ReadAll(reqjoke.Body)
+			var joke Joke
+			json.Unmarshal(body, &joke)
+			fmt.Println(joke.Value)
+		} else {
+			fmt.Println("Well thats too bad goodbye!")
+			break
+		}
+	}
 }
